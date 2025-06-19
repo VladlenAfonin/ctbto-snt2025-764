95c5744e50d14feca6c4b7084ede283f1e98fcbf
vladlenafonin <vladlenafonin.university@yandex.ru>
2025-06-20 00:39:47 +0300

    chore: use random data

--- src/ctbto/cli/main.py
+++ src/ctbto/cli/main.py
@@ -1,3 +1,5 @@
+import logging
+import logging.config
 import sys
 from time import time_ns
 from types import ModuleType
@@ -12,17 +14,39 @@ from Crypto.PublicKey import ECC
 import scienceplots
 
 
-def run_signature(
-    input_size: int = 4096,
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {"format": "%(levelname)s:%(name)s:%(funcName)s():%(message)s"}
+    },
+    "handlers": {
+        "stdout": {
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "loggers": {
+        "ctbto.cli.main": {"level": "INFO", "handlers": ["stdout"]},
+    },
+}
+
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger(__name__)
+
+
+def run_ecdsa(
+    input_size: int,
     hash_function: ModuleType = SHA256,
     n_iter: int = 100,
 ) -> numpy.float32:
-    secret = ECC.generate(curve="p256")
-    input = b"0" * input_size
-    signer = DSS.new(secret, "fips-186-3")
-
     times = []
     for _ in tqdm(range(n_iter), leave=False):
+        secret = ECC.generate(curve="p256")
+        signer = DSS.new(secret, "fips-186-3")
+        input = numpy.random.bytes(input_size)
+
         begin = time_ns()
 
         input_digest = hash_function.new(input)
@@ -35,22 +59,20 @@ def run_signature(
 
 
 def run_hmac(
-    secret_size: int = 20,
-    input_size: int = 4096,
+    input_size: int,
+    secret_size: int = 32,
     hash_function: ModuleType = SHA256,
     n_iter: int = 100,
 ) -> numpy.float32:
-    secret = b"0" * secret_size
-    input = b"0" * input_size
-    clean_hmac = HMAC.new(secret, digestmod=hash_function)
-
     times = []
     for _ in tqdm(range(n_iter), leave=False):
-        working_hmac = clean_hmac.copy()
+        secret = numpy.random.bytes(secret_size)
+        input = numpy.random.bytes(input_size)
+
         begin = time_ns()
 
-        working_hmac.update(input)
-        _ = working_hmac.digest()
+        hmac = HMAC.new(secret, input, digestmod=hash_function)
+        _ = hmac.digest()
 
         end = time_ns()
         times.append(end - begin)
@@ -58,27 +80,48 @@ def run_hmac(
     return numpy.mean(times)
 
 
+def megabytes_to_bytes(xs: numpy.ndarray) -> numpy.ndarray:
+    return (xs * 1024 * 1024).astype(numpy.int64)
+
+
+def bytes_to_megabytes(xs: numpy.ndarray) -> numpy.ndarray:
+    return xs / 1024 / 1024
+
+
 def main() -> int:
-    xs = numpy.linspace(0.1, 50.0, 10)
-    xs = numpy.int64(xs * 1024 * 1024)
+    xs = numpy.linspace(start=1.0, stop=10.0, num=20)
+    xs = megabytes_to_bytes(xs)
     n_iter = 10
 
-    run_hmac_vec = numpy.vectorize(
-        run_hmac,
-        excluded=["secret_size", "hash_function", "n_iter"],
-    )
-    ys_hmac = run_hmac_vec(input_size=xs, n_iter=n_iter) // 1_000_000
+    logger.info("begin benchmarking hmac-sha256")
+    ys_hmac = []
+    for x in tqdm(xs):
+        ys_hmac.append(
+            run_hmac(x, n_iter=n_iter) // 1_000_000,
+        )
+    ys_hmac = numpy.array(ys_hmac)
+    logger.info("end benchmarking hmac-sha256")
 
-    run_signature_vec = numpy.vectorize(
-        run_signature,
-        excluded=["hash_function", "n_iter"],
-    )
-    ys_ecdsa = run_signature_vec(input_size=xs, n_iter=n_iter) // 1_000_000
+    logger.info("begin benchmarking ecdsa")
+    ys_ecdsa = []
+    for x in tqdm(xs):
+        ys_ecdsa.append(
+            run_ecdsa(x, n_iter=n_iter) // 1_000_000,
+        )
+    ys_ecdsa = numpy.array(ys_ecdsa)
+    logger.info("end benchmarking ecdsa")
 
-    plt.style.use("science")
+    xs = bytes_to_megabytes(xs)
+
+    plt.style.use(["science"])
     _, ax = plt.subplots()
-    ax.plot(xs / 1024 / 1024, ys_hmac, label=r"$\textsf{HMAC}$")
-    ax.plot(xs / 1024 / 1024, ys_ecdsa, label=r"$\textsf{ECDSA}$")
+
+    ax.set_title("")
+    ax.set_xlabel("Input size, MB")
+    ax.set_ylabel("Generation time, ms")
+    ax.plot(xs, ys_hmac, label=r"$\textsf{HMAC-SHA256}$")
+    ax.plot(xs, ys_ecdsa, label=r"$\textsf{ECDSA-SHA256}$")
+
     plt.legend()
     plt.show()
 
